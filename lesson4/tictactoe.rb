657cd59cb2425d4bda908911fb65a338c9a158c0
Arman Jon Villalobos <me@armanjon.com>
2016-06-03 19:04:20 +0800

    make the board bigger

--- lesson4/tictactoe.rb
+++ lesson4/tictactoe.rb
@@ -6,11 +6,17 @@ end
 
 def display_board(board)
   puts ""
+  puts "     |     |"
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
+  puts "     |     |"
   puts "-----|-----|-----"
+  puts "     |     |"
   puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}"
+  puts "     |     |"
   puts "-----|-----|-----"
+  puts "     |     |"
   puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}"
+  puts "     |     |"
   puts ""
 end
 
