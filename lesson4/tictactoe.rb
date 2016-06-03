c75ce032942bdb4880c189c35dc7b24a2661ab05
Arman Jon Villalobos <me@armanjon.com>
2016-06-03 18:48:39 +0800

    added methods to display board

---
+++ lesson4/tictactoe.rb
@@ -0,0 +1,18 @@
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = 'O' }
+  new_board
+end
+
+def display_board(board)
+  puts ""
+  puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
+  puts "-----|-----|-----"
+  puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}"
+  puts "-----|-----|-----"
+  puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}"
+  puts ""
+end
+
+board = initialize_board
+display_board(board)
