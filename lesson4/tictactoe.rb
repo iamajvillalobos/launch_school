45de2a64eb03d4b18356287296d33abffe0c6446
Arman Jon Villalobos <me@armanjon.com>
2016-06-03 19:12:40 +0800

    add method for player to add a piece on the board

--- lesson4/tictactoe.rb
+++ lesson4/tictactoe.rb
@@ -9,16 +9,25 @@ def display_board(board)
   puts "     |     |"
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
   puts "     |     |"
-  puts "-----|-----|-----"
+  puts "-----+-----+-----"
   puts "     |     |"
   puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}"
   puts "     |     |"
-  puts "-----|-----|-----"
+  puts "-----+-----+-----"
   puts "     |     |"
   puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}"
-  puts "     |     |"
+  puts "     +     +"
   puts ""
 end
 
+def player_place_piece(board)
+  puts "Choose a square: (1-9)"
+  square = gets.chomp.to_i
+  board[square] = 'X'
+end
+
 board = initialize_board
+display_board(board)
+
+player_place_piece(board)
 display_board(board)
